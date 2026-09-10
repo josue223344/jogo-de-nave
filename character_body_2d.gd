45d683c24bf4bb5f5561ae05ceaf2830a05fcb1a
josue223344 <josuesousapaixao223@gmail.com>
2026-09-10 16:58:26 -0300

    adicionando nave

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,25 +1,23 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+@onready var sprite = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2.ZERO
 
 	move_and_slide()
+
+	# Animação parado
+	if velocity == Vector2.ZERO:
+		sprite.play("idle")
+	elif velocity.x < 0:
+		sprite.play("left")
+	elif velocity.x > 0:
+		sprite.play("right")
